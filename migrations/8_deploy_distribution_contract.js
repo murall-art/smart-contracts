6e00c18fcaa22199ef2e8e3b42f55021181a3e1f
Keiron Gulrajani <keiron.gulrajani@gmail.com>
2020-12-22 17:34:37 +0000

    - transfer tokens to distributor as part of migration

--- migrations/8_deploy_distribution_contract.js
+++ migrations/8_deploy_distribution_contract.js
@@ -1,7 +1,15 @@
 var MerkleDistributor = artifacts.require('./distribution/MerkleDistributor.sol');
 var Token = artifacts.require('./PaintToken.sol');
 
-module.exports = async function (deployer) {
-    const merkleRoot = '0xba29e8379d37df7225c311ba90176eb9d2e6de1f79e9b5ff115cf93cef63f818';
+module.exports = async function (deployer, network, accounts) {
+    const merkleRoot = '0x75984c7647bf9a87f37a6b1d9c8d23dbcc461bc3f1678945ce95aa9138a8cdb2';
+    // 18060697031000000000000000000 tokens to transfer
+    const totalTokenToTransferIntoDistributor = "0x00000000000000000000000000000000000000003a5b755be1714b7672bc0000";
     await deployer.deploy(MerkleDistributor, Token.address, merkleRoot);
+
+    paintTokenInstance = await Token.deployed();
+    MerkleDistributorInstance = await MerkleDistributor.deployed();
+    paintTokenInstance.transfer(MerkleDistributorInstance.address, totalTokenToTransferIntoDistributor, {
+        from: accounts[0],
+    });
 };
