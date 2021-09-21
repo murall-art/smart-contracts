87c0e4df4b965e58d1edce640c0e52a975fe7ab8
Keiron <keiron.gulrajani@gmail.com>
2021-09-21 12:00:03 +0100

    Added code to set/unset frame contents

--- contracts/frames/MurAllFrame.sol
+++ contracts/frames/MurAllFrame.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/introspection/ERC165Checker.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155Receiver.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
@@ -15,9 +19,47 @@ import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 /**
  * MurAll Frame contract
  */
-contract MurAllFrame is ERC721, Ownable, AccessControl, ReentrancyGuard, IERC2981, VRFConsumerBase {
+contract MurAllFrame is
+    ERC721,
+    Ownable,
+    AccessControl,
+    ReentrancyGuard,
+    IERC2981,
+    VRFConsumerBase,
+    IERC721Receiver,
+    ERC1155Receiver
+{
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     using Strings for uint256;
+    using ERC165Checker for address;
+    /*
+     *     bytes4(keccak256('balanceOf(address)')) == 0x70a08231
+     *     bytes4(keccak256('ownerOf(uint256)')) == 0x6352211e
+     *     bytes4(keccak256('approve(address,uint256)')) == 0x095ea7b3
+     *     bytes4(keccak256('getApproved(uint256)')) == 0x081812fc
+     *     bytes4(keccak256('setApprovalForAll(address,bool)')) == 0xa22cb465
+     *     bytes4(keccak256('isApprovedForAll(address,address)')) == 0xe985e9c5
+     *     bytes4(keccak256('transferFrom(address,address,uint256)')) == 0x23b872dd
+     *     bytes4(keccak256('safeTransferFrom(address,address,uint256)')) == 0x42842e0e
+     *     bytes4(keccak256('safeTransferFrom(address,address,uint256,bytes)')) == 0xb88d4fde
+     *
+     *     => 0x70a08231 ^ 0x6352211e ^ 0x095ea7b3 ^ 0x081812fc ^
+     *        0xa22cb465 ^ 0xe985e9c ^ 0x23b872dd ^ 0x42842e0e ^ 0xb88d4fde == 0x80ac58cd
+     */
+    bytes4 private constant _INTERFACE_ID_ERC721 = 0x80ac58cd;
+
+    /*
+     *     bytes4(keccak256('balanceOf(address,uint256)')) == 0x00fdd58e
+     *     bytes4(keccak256('balanceOfBatch(address[],uint256[])')) == 0x4e1273f4
+     *     bytes4(keccak256('setApprovalForAll(address,bool)')) == 0xa22cb465
+     *     bytes4(keccak256('isApprovedForAll(address,address)')) == 0xe985e9c5
+     *     bytes4(keccak256('safeTransferFrom(address,address,uint256,uint256,bytes)')) == 0xf242432a
+     *     bytes4(keccak256('safeBatchTransferFrom(address,address,uint256[],uint256[],bytes)')) == 0x2eb2c2d6
+     *
+     *     => 0x00fdd58e ^ 0x4e1273f4 ^ 0xa22cb465 ^
+     *        0xe985e9c5 ^ 0xf242432a ^ 0x2eb2c2d6 == 0xd9b67a26
+     */
+    bytes4 private constant _INTERFACE_ID_ERC1155 = 0xd9b67a26;
 
     IFrameImageStorage public traitImageStorage;
     IRoyaltyGovernor public royaltyGovernorContract;
@@ -30,6 +72,14 @@ contract MurAllFrame is ERC721, Ownable, AccessControl, ReentrancyGuard, IERC298
     uint256 constant MAX_SUPPLY = 2100;
 
     uint256[] internal initialFrameTraits;
+
+    struct FrameContents {
+        address contractAddress;
+        uint256 tokenId;
+        uint256 amount;
+    }
+
+    mapping(uint256 => FrameContents) private frameContents;
 
     // for chainlink vrf
     bytes32 internal keyHash;
@@ -49,11 +99,31 @@ contract MurAllFrame is ERC721, Ownable, AccessControl, ReentrancyGuard, IERC298
      */
     string private viewUriBase;
 
+    /** @dev check supports erc721 or erc1155 using eip165
+     * @param tokenContractAddress Contract address to check if is ERC721 or ERC1155
+     */
+    modifier onlySupportedNfts(address tokenContractAddress) {
+        require(
+            tokenContractAddress.supportsInterface(_INTERFACE_ID_ERC721) ||
+                tokenContractAddress.supportsInterface(_INTERFACE_ID_ERC1155),
+            "Contract is not ERC721 or ERC1155"
+        );
+        _;
+    }
+
     /** @dev Checks if token exists
      * @param _tokenId The token id to check if exists
      */
     modifier onlyExistingTokens(uint256 _tokenId) {
         require(_exists(_tokenId), "Invalid Token ID");
+        _;
+    }
+
+    /** @dev Checks if token owner is sender
+     * @param _tokenId The token id to check
+     */
+    modifier onlyTokenOwner(uint256 _tokenId) {
+        require(ownerOf(_tokenId) == msg.sender, "Invalid Token ID");
         _;
     }
 
@@ -65,7 +135,13 @@ contract MurAllFrame is ERC721, Ownable, AccessControl, ReentrancyGuard, IERC298
     }
 
     event FrameMinted(uint256 indexed id, address indexed owner);
-    event FrameTraitsUpdated(uint256 indexed id, address indexed owner, uint256 traits);
+    event FrameContentsUpdated(
+        uint256 indexed id,
+        address indexed contentsContract,
+        uint256 contentsId,
+        uint256 amount
+    );
+    event FrameContentsRemoved(uint256 indexed id);
     event RoyaltyGovernorContractChanged(address indexed royaltyGovernor);
     event FrameTraitImageStorageContractChanged(address indexed traitImageStorage);
 
@@ -169,6 +245,136 @@ contract MurAllFrame is ERC721, Ownable, AccessControl, ReentrancyGuard, IERC298
 
     function setPublicMintingEnabled(bool enabled) public onlyAdmin {
         publicMintingEnabled = enabled;
+    }
+
+    function setPresaleMintingEnabled(bool enabled) public onlyAdmin {
+        presaleMintingEnabled = enabled;
+    }
+
+    /**
+     * @dev See {IERC721Receiver-onERC721Received}.
+     *
+     * Always returns `IERC721Receiver.onERC721Received.selector`.
+     */
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        require(from == msg.sender, "Invalid sender");
+        require(data.length != 0, "Invalid data - must contain target frame token id");
+        (uint256 targetFrameTokenId, address owner, address contractAddress) = abi.decode(
+            data,
+            (uint256, address, address)
+        );
+        require(_exists(targetFrameTokenId), "Invalid Token ID");
+        require(owner == msg.sender, "Invalid owner");
+        require(ownerOf(targetFrameTokenId) == msg.sender, "Invalid owner");
+        require(!hasContentsInFrame(targetFrameTokenId), "Frame already contains an NFT");
+        require(
+            contractAddress.supportsInterface(_INTERFACE_ID_ERC721) &&
+                IERC721(contractAddress).ownerOf(tokenId) == address(this),
+            "Incorrect data"
+        );
+        FrameContents memory newFrameContents = FrameContents(contractAddress, tokenId, 1);
+        frameContents[targetFrameTokenId] = newFrameContents;
+
+        emit FrameContentsUpdated(targetFrameTokenId, contractAddress, tokenId, 1);
+        return this.onERC721Received.selector;
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        uint256 amount,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        require(from == msg.sender, "Invalid sender");
+        require(data.length != 0, "Data must contain target frame id, owner and contract address");
+        (uint256 targetFrameTokenId, address owner, address contractAddress) = abi.decode(
+            data,
+            (uint256, address, address)
+        );
+
+        require(!hasContentsInFrame(targetFrameTokenId), "Frame already contains an NFT"); // Also checks token exists
+        require(owner == msg.sender, "Invalid owner");
+        require(ownerOf(targetFrameTokenId) == msg.sender, "Invalid owner");
+        require(
+            contractAddress.supportsInterface(_INTERFACE_ID_ERC1155) &&
+                IERC1155(contractAddress).balanceOf(address(this), tokenId) == amount,
+            "Incorrect data"
+        );
+        FrameContents memory newFrameContents = FrameContents(contractAddress, tokenId, amount);
+        frameContents[targetFrameTokenId] = newFrameContents;
+
+        emit FrameContentsUpdated(targetFrameTokenId, contractAddress, tokenId, 1);
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] memory,
+        uint256[] memory,
+        bytes memory
+    ) public virtual override returns (bytes4) {
+        revert();
+    }
+
+    function setFrameContents(
+        uint256 _tokenId,
+        address contentContractAddress,
+        uint256 contentTokenId,
+        uint256 contentAmount
+    ) public onlyTokenOwner(_tokenId) onlySupportedNfts(contentContractAddress) {
+        require(!hasContentsInFrame(_tokenId), "Frame already contains an NFT"); // Also checks token exists
+        // use contract address to get contract instance as ERC721 instance
+        if (contentContractAddress.supportsInterface(_INTERFACE_ID_ERC721)) {
+            // transfer ownership of the token to this contract (will fail if contract is not approved prior to this)
+            IERC721(contentContractAddress).safeTransferFrom(
+                msg.sender,
+                address(this),
+                contentTokenId,
+                abi.encode(_tokenId, msg.sender, contentContractAddress)
+            );
+        } else {
+            // transfer ownership of the token to this contract (will fail if contract is not approved prior to this)
+            IERC1155(contentContractAddress).safeTransferFrom(
+                msg.sender,
+                address(this),
+                contentTokenId,
+                contentAmount,
+                abi.encode(_tokenId, msg.sender, contentContractAddress)
+            );
+        }
+    }
+
+    function removeFrameContents(uint256 _tokenId) public onlyTokenOwner(_tokenId) {
+        require(hasContentsInFrame(_tokenId), "Frame does not contain an NFT"); // Also checks token exists
+        FrameContents memory _frameContents = frameContents[_tokenId];
+        if (_frameContents.contractAddress.supportsInterface(_INTERFACE_ID_ERC721)) {
+            // transfer ownership of the token to this contract (will fail if contract is not approved prior to this)
+            IERC721(_frameContents.contractAddress).safeTransferFrom(address(this), msg.sender, _frameContents.tokenId);
+        } else {
+            // transfer ownership of the token to this contract (will fail if contract is not approved prior to this)
+            IERC1155(_frameContents.contractAddress).safeTransferFrom(
+                address(this),
+                msg.sender,
+                _frameContents.tokenId,
+                _frameContents.amount,
+                ""
+            );
+        }
+
+        delete frameContents[_tokenId];
+        emit FrameContentsRemoved(_tokenId);
+    }
+
+    function hasContentsInFrame(uint256 _tokenId) public view onlyExistingTokens(_tokenId) returns (bool) {
+        // if seller profile does not exist, create one
+        return frameContents[_tokenId].contractAddress != address(0);
     }
 
     /**
